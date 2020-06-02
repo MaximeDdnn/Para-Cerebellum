5bbf1119c9293ba12a6afaed2c732a6d7bc5f84d
MaximeDdnn <62238305+MaximeDdnn@users.noreply.github.com>
2020-06-02 17:09:09 +0200

    mosaic display

---
+++ display.py
@@ -0,0 +1,18 @@
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+import PIL
+
+
+def display_mosaic(list_slices, slices):
+    nb_img = len(list_slices)
+    rows = 2
+
+    for x in range(1,nb_img+1):
+        img = list_slices[x-1]
+        plt.subplot(rows, 5, x)
+        str = 'slice %d' % slices[x-1]
+        plt.title(str)
+        plt.axis('off')
+        plt.imshow(img)
+    plt.show()
