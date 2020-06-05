435ffbbe3d4270debb9e6ff17cff2cecf18fc55b
MaximeDdnn <maximedieudonne@protonmail.com>
2020-06-06 00:16:00 +0200

    test templating

--- jinja_env.py
+++ jinja_env.py
@@ -1,16 +1,21 @@
 from jinja2 import Environment, FileSystemLoader
 from weasyprint import HTML
+import pandas as pd
+import nibabel as nib
+import matplotlib.pyplot as plt
 
 # templating
 env = Environment(loader=FileSystemLoader('.'))
 template = env.get_template("template.html")
 
-template_vars = {"visuel_image" : "lalal",
-                 "recap_image" : "lolo"}
+tab = pd.read_csv('lut_perso.csv')
+
+
+template_vars = {
+    "recap_image": tab.to_html()}
 
 html_out = template.render(template_vars)
 
 # generate PDF
-HTML(string=html_out).write_pdf("report.pdf")
-
+HTML(string=html_out, base_url='.').write_pdf("report3.pdf", stylesheets=["typography.css"])
 
