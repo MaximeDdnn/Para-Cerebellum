88bea8e086874ef5bfe8e4ed30f5b546b9f59f4e
MaximeDdnn <maximedieudonne@protonmail.com>
2020-06-16 09:50:08 +0200

    correction output path

--- CNN/script_cnn.py
+++ CNN/script_cnn.py
@@ -2,11 +2,16 @@
 
 import os
 import subprocess
+import shutil
 
+"""
+script  for run the CNN programme avalailable here : http://www.iacl.ece.jhu.edu/index.php/Cerebellum_CNN
+"""
 
 simg_path = '/home/dieudonnem/hpc/soft_cerebellum/CNN/cerebellum-parcellation_v2.simg'
 input_dir = '/home/dieudonnem/hpc/data/dataset_sence'
-output_dir = '/home/dieudonnem/hpc/out/CNN'
+output_folder = '/CNN'  # output folder is created in the input folder by the cmd_cnn, that's why i move it after in my output_dir
+output_dir = '/home/dieudonnem/hpc/out'
 files = os.listdir(input_dir)
 files.sort()
 print(files)
@@ -15,4 +20,6 @@ for mri in files:
     output_folder = os.path.join(output_dir, output_folder_name)
     cmd_cnn = 'singularity run -B %s:/mnt %s -i /mnt/%s -o /mnt/%s' % (input_dir, simg_path, mri, output_folder)
     subprocess.run(cmd_cnn, shell=True)
+shutil.move(os.path.join(input_dir, output_folder), output_dir)
+
 
--- suit/check_dif.py
+++ suit/check_dif.py
@@ -1,6 +1,11 @@
 import numpy as np
 import nibabel as nib
 
+"""
+research code to check if two image are the same or not
+
+"""
+
 img1_path = '/home/dieudonnem/hpc/out/suit/dataset_sence/pred/Folder_r09_sub-testanat2_acq-0p8mm_rec-uniden8000_T1w/iw_Lobules-SUIT_u_a_r09_sub-testanat2_acq-0p8mm_rec-uniden8000_T1w_seg1.nii'
 img2path = '/home/dieudonnem/hpc/out/suit/dataset_sence/pred/T1T2/Folder_r09_sub-testanat2_acq-0p8mm_rec-uniden8000_T1w/iw_Lobules-SUIT_u_a_r09_sub-testanat2_acq-0p8mm_rec-uniden8000_T1w_seg1.nii'
 img1 = nib.load(img1_path)
