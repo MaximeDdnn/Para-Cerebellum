207e69401f799c2011c2652ddc5b4a876ef6a352
MaximeDdnn <maximedieudonne@protonmail.com>
2020-07-08 17:33:09 +0200

    new visualisation function added

--- comparaison/dice.py
+++ comparaison/dice.py
@@ -47,8 +47,6 @@ def minimize_label(input_dir, softs,  edge_flag=True):
 
 
 
-
-
 def save_dice_matrix(list_img, soft1, soft2, soft1_mask_path, soft2_mask_path, dict_label):
     """
     :param list_img:
@@ -86,6 +84,57 @@ def save_dice_matrix(list_img, soft1, soft2, soft1_mask_path, soft2_mask_path, d
 
 
 
+def split_dice_matrix(dice_matrix, label):
+    """
+    split the dice matrix that contain all the area of the cerebellum in two smaller dice matrix for a better interpretability.
+    it splits into a dice matrix without left areaw and in an other dice matrix without right areas
+    :param dice_matrix: dice matrix that contains all the area
+    :param label: dict with lab
+    :return: dice_matrix_L for left ( and vermis) and dice matric_R for right (and vermis)
+    """
+    idx_noR = [label.index(x) for x in label if 'R_' not in x]
+    idx_noL = [label.index(x) for x in label if 'L_' not in x]
+    dice_matrix_R = dice_matrix[idx_noL,:][:,idx_noL]
+    dice_matrix_L = dice_matrix[idx_noR, :][:, idx_noR]
+    return dice_matrix_L, dice_matrix_R
+
+def displayRL(soft1, soft2, list_label_name_L,list_label_name_R):
+    input_dir = os.path.join('/home/dieudonnem/hpc/out/comparaison', soft1 + '_' + soft2, 'dataset_sence/sub-1/dice_matrix')
+    subfolders = os.listdir(input_dir)
+    subfolders.sort()
+    for folder in subfolders:
+        fig, axs = plt.subplots(1, 2)
+        dice_matrix_L = np.load(os.path.join(input_dir, folder, 'dice_matrix_L.npy'))
+        im0 = axs[0].imshow(dice_matrix_L)
+        axs[0].set_xticks(np.arange(len(list_label_name_L)))
+        axs[0].set_yticks(np.arange(len(list_label_name_L)))
+        axs[0].set_xticklabels(list_label_name_L)
+        axs[0].set_yticklabels(list_label_name_L)
+        plt.setp(axs[0].get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor", fontsize = "x-small")
+        plt.setp(axs[0].get_yticklabels(), fontsize="x-small")
+        for i in range(len(list_label_name_L)):
+            for j in range(len(list_label_name_L)):
+                text = axs[0].text(j, i, round(dice_matrix_L[i, j], 2), ha="center", va="center", color="w", fontsize = "xx-small")
+        axs[0].set_title('Left Hemisphere')
+        dice_matrix_R = np.load(os.path.join(input_dir, folder, 'dice_matrix_R.npy'))
+        im1 = axs[1].imshow(dice_matrix_R)
+        axs[1].set_xticks(np.arange(len(list_label_name_R)))
+        axs[1].set_yticks(np.arange(len(list_label_name_R)))
+        axs[1].set_xticklabels(list_label_name_R)
+        axs[1].set_yticklabels(list_label_name_R)
+        plt.setp(axs[1].get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor", fontsize="x-small")
+        plt.setp(axs[1].get_yticklabels(), fontsize="x-small")
+        for i in range(len(list_label_name_R)):
+            for j in range(len(list_label_name_R)):
+                text = axs[1].text(j, i, round(dice_matrix_R[i, j], 2), ha="center", va="center", color="w",
+                               fontsize="xx-small")
+        axs[1].set_title('Right Hemisphere')
+        fig.suptitle(folder)
+        fig.tight_layout()
+        plt.show()
+
+
+
 def display(soft1, soft2, list_label_name_soft1,list_label_name_soft2):
     input_dir = os.path.join('/home/dieudonnem/hpc/out/comparaison', soft1 + '_' + soft2, 'dataset_sence/sub-1/dice_matrix')
     subfolders = os.listdir(input_dir)
@@ -106,6 +155,44 @@ def display(soft1, soft2, list_label_name_soft1,list_label_name_soft2):
         ax.set_title(folder)
         fig.tight_layout()
         plt.show()
+
+
+
+def exl(label_R):
+    """
+    create an excel sheet that sum up dice score for each area ( column ) and each type of record ( row )
+    :return:
+    """
+    input_path = os.path.join('/home/dieudonnem/hpc/out/comparaison', 'suit_cnn', 'dataset_sence/sub-1/dice_matrix')
+    list_folder = os.listdir(input_path)
+    list_dice_matrix = [np.load(os.path.join(input_path, folder, 'dice_matrix_R.npy')) for folder in list_folder]
+    dice_rec = np.array([np.diag(x) for x in list_dice_matrix])
+    print(type(dice_rec))
+    print(dice_rec)
+    # display
+    fig, ax = plt.subplots()
+    ax.set_xticks(np.arange(len(list_folder)))
+    ax.set_xticklabels(list_folder)
+    for folder in range(len(label_R)):
+        plt.plot(dice_rec[:, folder], 'o-', label=label_R[folder])
+    plt.setp(ax.get_xticklabels(), rotation=10, ha="right", rotation_mode="anchor", fontsize="xx-small")
+    plt.legend(loc = 0)
+    plt.show()
+
+
+def comp_rec():
+    input_path = '/home/dieudonnem/hpc/out/comparaison/suit_cnn/dataset_sence/sub-1/dice_matrix'
+    list_folder = os.listdir(input_path)
+    fig, ax = plt.subplots()
+    x = np.arange(len(list_folder))
+    ax.set_xticks(np.arange(len(x)))
+    ax.set_yticks(np.arange(len(list_folder)))
+    ax.set_xticklabels(list_folder)
+    ax.set_yticklabels(x)
+    for a in range(5):
+        plt.plot(x+a, 'o-')
+    plt.setp(ax.get_xticklabels(), rotation=10, ha="right", rotation_mode="anchor", fontsize="xx-small")
+    plt.show()
 
 # main
 
@@ -129,6 +216,8 @@ list_img = os.listdir(suiter_path)
 list_img.sort()
 
 list_label_name = list(label_suiter_cnn.keys())
+list_label_name_R = [x for x in list_label_name if 'R_' not in x]
+list_label_name_L = [x for x in list_label_name if 'L_' not in x]
 list_label_name_suiter = ["suiter " + x for x in list_label_name]
 list_label_name_cnn = ["cnn " + x for x in list_label_name]
 
@@ -138,6 +227,20 @@ list_label_name_cnn = ["cnn " + x for x in list_label_name]
 
 
 #save_dice_matrix(list_img, 'suit', 'cnn', suit_path, cnn_path, label_suiter_cnn)
-display('suiter', 'cnn', list_label_name_suiter, list_label_name_cnn)
+#display('suiter', 'cnn', list_label_name_suiter, list_label_name_cnn)
 
 
+
+# input_path = '/home/dieudonnem/hpc/out/comparaison/suit_cnn/dataset_sence/sub-1/dice_matrix'
+# list_folder = os.listdir(input_path)
+# list_folder.sort()
+# for folder in list_folder:
+#     dice_matrix = np.load(os.path.join(input_path, folder, 'dice_matrix.npy'))
+#     dice_matrix_L, dice_matrix_R = split_dice_matrix(dice_matrix, list(label_suiter_cnn.keys()))
+#     np.save(os.path.join(input_path, folder, 'dice_matrix_L.npy'), dice_matrix_L,)
+#     np.save(os.path.join(input_path, folder, 'dice_matrix_R.npy'), dice_matrix_R)
+
+#displayRL('suit', 'cnn', list_label_name_L, list_label_name_R)
+
+#comp_rec()
+exl(list_label_name_R)
